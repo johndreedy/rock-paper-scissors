1a9de4ab7972e79e9fe20da09a36683a1d41821f
John Reedy <johnreedy01@gmail.com>
2023-09-16 19:23:45 +0100

    Complete logic for single round RPS game

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
-let playerSelection = "Rock";
+let playerSelection = prompt("Rock, paper or scissors?");
 let computerSelection = getComputerChoice();
+
+let playerPointScore = 0;
+let computerPointScore = 0;
 
 function getPlayerChoice (playerInput) {
     let caseInsensitiveInput = playerInput.toLowerCase();
-    return caseInsensitiveInput;
+    let capitalisedInput = caseInsensitiveInput.charAt(0).toUpperCase() + caseInsensitiveInput.slice(1);
+    return capitalisedInput;
 }
 
 function getComputerChoice () {
@@ -14,17 +18,48 @@ function getComputerChoice () {
 }
 
 function playRound (playerSelection, computerSelection) {
-    switch(playerSelection) {
-        case "rock":
-            break;
-        case "paper":
-            break;
-        case "scissors":
-            break;
+    playerSelection = getPlayerChoice(playerSelection);
+    switch (playerSelection) {
+        case "Rock":
+            if (computerSelection == "Scissors") {
+                alert(`You win! ${playerSelection} cuts ${computerSelection}.`)
+                return playerPointScore += 1;
+            } else if (computerSelection == "Paper") {
+                alert(`You lose! ${computerSelection} wraps ${playerSelection}.`)
+                return computerPointScore += 1;
+            } else {
+                 alert(`Draw! ${playerSelection} is equal to ${computerSelection}. Play again!`)
+                break;
+            }
+
+        case "Paper":
+            if (computerSelection == "Rock") {
+                alert(`You win! ${playerSelection} wraps ${computerSelection}.`)
+                return playerPointScore += 1;
+            } else if (computerSelection == "Scissors") {
+                alert(`You lose! ${computerSelection} blunts ${playerSelection}.`)
+                return computerPointScore += 1;
+            } else {
+                alert(`Draw! ${playerSelection} is equal to ${computerSelection}. Play again!`)
+                break;
+            }
+        
+        case "Scissors":
+            if (computerSelection == "Paper") {
+                alert(`You win! ${playerSelection} cuts ${computerSelection}.`)
+                return playerPointScore += 1;
+            } else if (computerSelection == "Rock") {
+                alert(`You lose! ${computerSelection} blunts ${playerSelection}.`)
+                return computerPointScore += 1;
+            } else {
+                alert(`Draw! ${playerSelection} is equal to ${computerSelection}. Play again!`)
+                break;
+            }
         default:
-            alert("Invalid value entered")
+            alert("Invalid value entered.")
             break;
     }
 }
 
-console.log(getPlayerChoice(playerSelection));
+playRound(playerSelection, computerSelection);
+
