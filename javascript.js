af44cccdcb422641280a78dcb33284f05c2d748b
John Reedy <johnreedy01@gmail.com>
2023-09-16 17:03:52 +0100

    Add case insensitive input and basic playRound outline

--- javascript.js
+++ javascript.js
@@ -1,8 +1,30 @@
-function getComputerChoice () {
-    const computerOptions = ["Rock", "Paper", "Scissors"];
-    let randomChoiceIndex = Math.floor(Math.random() * computerOptions.length);
-    let computerChoice = computerOptions[randomChoiceIndex];
-    return computerChoice;
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice();
+
+function getPlayerChoice (playerInput) {
+    let caseInsensitiveInput = playerInput.toLowerCase();
+    return caseInsensitiveInput;
 }
 
-console.log(getComputerChoice());
+function getComputerChoice () {
+    const computerOptions = ["Rock", "Paper", "Scissors"];
+    let randomSelectionIndex = Math.floor(Math.random() * computerOptions.length);
+    let computerSelection = computerOptions[randomSelectionIndex];
+    return computerSelection;
+}
+
+function playRound (playerSelection, computerSelection) {
+    switch(playerSelection) {
+        case "rock":
+            break;
+        case "paper":
+            break;
+        case "scissors":
+            break;
+        default:
+            alert("Invalid value entered")
+            break;
+    }
+}
+
+console.log(getPlayerChoice(playerSelection));
